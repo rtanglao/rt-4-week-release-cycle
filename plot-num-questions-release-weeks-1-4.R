9a1e70b450f221e28ea2959984161439b2cd593f
Roland Tanglao <roland@rolandtanglao.com>
2019-10-13 08:12:30 -0700

    more work in progress

--- plot-num-questions-release-weeks-1-4.R
+++ plot-num-questions-release-weeks-1-4.R
@@ -1,2 +1,8 @@
 library(tidyverse)
 jan_aug_2019_questions <- read_csv("https://raw.githubusercontent.com/rtanglao/rt-kitsune-api/master/01jan2019-31aug2019.csv")
+# read in the release json file, argv(1)
+# read in the file withi the releases argv(2)
+# see https://github.com/rtanglao/rt-kitsune-api/blob/master/VISUALIZATIONS/add-release-day-number.r
+# see https://github.com/rtanglao/rt-kitsune-api/blob/master/VISUALIZATIONS/add-release-week-number.r
+# see plot-antivirus-mentions-by-3weeks.r which is:
+# https://github.com/rtanglao/rt-kitsune-api/blob/master/VISUALIZATIONS/plot-antivirus-mentions-by-3weeks.r
