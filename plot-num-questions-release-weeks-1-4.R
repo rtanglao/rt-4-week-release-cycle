554bac325500eb750051f1a007bbdee123a1f050
Roland Tanglao <roland@rolandtanglao.com>
2019-10-18 22:13:16 -0700

    change unixtime to datetime

--- plot-num-questions-release-weeks-1-4.R
+++ plot-num-questions-release-weeks-1-4.R
@@ -1,7 +1,35 @@
 library(tidyverse)
+library(lubridate)
+library(anytime)
+add_release_week_number
+<-
+  function(df_release,
+           yyyy,
+           mm,
+           dd)
+  {
+    START_DATE <-
+      make_datetime(yyyy, mm, dd, 0, 0, 0,
+                    tz = "UTC")
+    return (df_release %>%
+              mutate(release_week_number =
+                       floor(interval(
+                         START_DATE, created
+                       ) / days(7)) + 1))
+  }
 jan_aug_2019_questions <- read_csv("https://raw.githubusercontent.com/rtanglao/rt-kitsune-api/master/01jan2019-31aug2019.csv")
+jan_aug_2019_questions <- jan_aug_2019_questions %>%
+   mutate(
+    created = as_datetime(created)
+     )
+# change created unix time to r time UTC using as_datetime()
+
+# Firefox 65, January 29, 2019
+# FF65W1D1, FF65W1D2, FF65W1D3, FF65W1D4, FF65W1D5,FF65W1D6,FF65W1D7
+# FF65W2D1, FF65W2D2, FF65W2D3, FF65W2D4, FF65W2D5,FF65W2D6,FF65W2D7
+# We want a column called "ReleaseWeekDay" with values 1-7
 # read in the release json file, argv(1)
-# read in the file withi the releases argv(2)
+# read in the file with the releases argv(2)
 # see https://github.com/rtanglao/rt-kitsune-api/blob/master/VISUALIZATIONS/add-release-day-number.r
 # see https://github.com/rtanglao/rt-kitsune-api/blob/master/VISUALIZATIONS/add-release-week-number.r
 # see plot-antivirus-mentions-by-3weeks.r which is:
